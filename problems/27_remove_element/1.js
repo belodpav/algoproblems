805ac69b5ef4e021b3ee2b2693bb266be1b9aaee
Pavel Belodurin <belod.pav@gmail.com>
2017-11-26 20:35:29 +0300

    Problem 27. Remove Element

---
+++ problems/27_remove_element/1.js
@@ -0,0 +1,8 @@
+var removeElement = function(nums, val) {
+    for (let i = nums.length; i >= 0; i--) {
+      if (nums[i] === val) {
+        nums.splice(i, 1);
+      }
+    }
+  return nums.length;
+};
